d594a6db48779ff7b6bd44ae54cb1377ae9d3952
Halory <2082004203@qq.com>
2026-03-20 15:36:31 +0800

    feat: add 关于保研了快半年我似乎还啥都没怎么学这件事.md

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,9 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "Kakaraku",
+	subtitle: "^_^",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -53,9 +53,9 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Karaku",
+	bio: "这个人很懒，什么都没有留下~",
 	links: [
 		{
 			name: "Twitter",
@@ -72,7 +72,17 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/Halory-Ito",
+		},
+		{
+			name: "QQ",
+			icon: "fa6-brands:qq",
+			url: "https://im.qq.com",
+		},
+		{
+			name: "Reddit",
+			icon: "fa6-brands:reddit",
+			url: "https://www.reddit.com",
 		},
 	],
 };
